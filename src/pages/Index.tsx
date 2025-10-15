e45467b086fa827788c591de3f170e6bbb9eaab3
Clément Palézis <palezis.C@gmail.com>
2025-10-16 00:20:17 +0200

    refactor(Index.tsx): temporarily hide projects section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
         <Hero />
         <About />
         <Skills />
-        <Projects />
+        {/* <Projects /> */}
         <Experience />
         <Contact />
       </main>
